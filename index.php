f31f9dc486872c23918615150b4b4d2fb5cc1cff
Daniel Ochoa Aguila <dochoa-aguila@csumb.edu>
2018-02-01 01:26:00 +0000

    First commit, portfolio lab

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset = "utf-8"/>
+        <title>Daniel Ochoa: Personal Website</title>
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Daniel Ochoa </h1>
+            <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        </header>
+        <nav>
+            <hr width = "50%" />
+            <a href = "index.html" id = "standOut">Home</a>
+            <a href = "about.html">About</a>
+            <a href = "contact.html">Contact</a>
+        </nav>
+    
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        
+        <main>
+            <figure id ="me">
+                <img src="img/juan_doe.png"/>
+            </figure>
+            <div id="welcomeText">
+                Hello!<br/>
+                <p>
+                    Thank you for visiting my professional profile!
+                </p>
+                <p>
+                    I have just been hired by Apple
+                </p>
+                <p>
+                    Feel free to contact me
+                </p>
+                <br/><br/>
+                <em>"with ordinaty talent and extrodinary......</em>
+            </div>
+        </main>
+        <footer>
+            <hr>
+            course name. 2018<br/>
+            <img src ="img/csumb.jpg"/>
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
